b2861f6979a39d27662ac7cbebd5e141fbc2e7c5
suhail-op <suhailfazil@opstimus.com>
2026-02-09 17:45:16 +0530

    Make tags variable

--- main.tf
+++ main.tf
@@ -2,10 +2,7 @@ resource "aws_acm_certificate" "main" {
   domain_name               = var.domain
   subject_alternative_names = var.wildcard == true ? ["*.${var.domain}"] : null
   validation_method         = "DNS"
-
-  tags = {
-    Environment = "${var.project}-${var.environment}"
-  }
+  tags                      = var.tags
 
   lifecycle {
     create_before_destroy = true
